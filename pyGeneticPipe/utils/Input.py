7267d588a822ee78dae4cb310fecb53587e65878
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-10-22 17:12:52 +0100

    set the summary headers for non-freqencies

--- pyGeneticPipe/utils/Input.py
+++ pyGeneticPipe/utils/Input.py
@@ -49,3 +49,89 @@ class Input:
         assert bim.exists(), ec.path_invalid(bim, "_set_ld_ref")
         assert fam.exists(), ec.path_invalid(fam, "_set_ld_ref")
         return "plink", None, bed, bim, fam
+
+    def _set_summary_stats(self, summary_stats_path):
+        if not summary_stats_path:
+            return None
+
+        # Construct path as an object
+        summary_path = Path(summary_stats_path)
+
+        # Check file exists
+        assert summary_path.exists(), ec.path_invalid(summary_path, "_set_summary_stats")
+
+        # Determine if file is g-zipped
+        gz_status = (summary_path.suffix == ".gz")
+
+        with mc.open_setter(summary_path)(summary_path) as file:
+            headers = self._validate_summary_headers(file.readline(), gz_status)
+            #
+            # print(headers)
+            #
+            # for index, line in enumerate(file):
+            #     print(index, line)
+            #     line = mc.decode_line(line, gz_status)
+            #     print(line)
+            #     print(line[headers["SNP"]])
+            #
+            #     break
+
+
+        return 0
+
+    def _validate_summary_headers(self, header_line, zip_status):
+        # Extract the raw headers
+        headers = mc.decode_line(header_line, zip_status)
+        header_dict = {}
+
+        # Determine if we have custom headers or not via _summary_headers
+        summary_headers = self._summary_headers
+        mandatory_headers = ["SNP_ID", "Effect_Allele", "Alt_Allele", "Effect_size", "P_Value"]
+
+        for summary_header in summary_headers:
+            print(summary_header)
+            header_indexes = [i for i, h in enumerate(headers) if h == summary_headers[summary_header]]
+            print(header_indexes)
+
+            assert len(header_indexes) < 2, ec
+            if len(header_indexes) == 0:
+                assert summary_header not in mandatory_headers, ec
+                header_dict[summary_header] = None
+            else:
+                header_dict[summary_header] = header_indexes[0]
+
+        print(header_dict)
+
+        header_dict = {h: i for i, h in enumerate(headers)}
+
+        return header_dict
+
+    @property
+    def _summary_headers(self):
+        """
+        We may have users using custom headers, or they may be using a format we already have covered
+
+        Note
+        -----
+        In GUI we basically want to call the header check to make sure we align columns correctly. If not they can set
+        it themselves
+
+        :return: The headers to validate
+        """
+        if self._args["Summary_Headers"]:
+            return self._args["Summary_Headers"]
+        else:
+            # Based on known summary statistics from LDPred sum_stats_parsers.py
+            header_sets = {
+                "Chromosome": ["CHR", "chr", "hg19chrc"],
+                "SNP_ID": ["SNP_ID", "rs", "snpid", "MarkerName", "SNP"],
+                "Effect_Allele": ["REF", "ref", "a1", "A1", "Allele1"],
+                "Alt_Allele": ["ALT", "alt", "a2", "A2", "Allele2"],
+                "Position": ["POS", "pos", "bp", "BP"],
+                "P_Value": ["PVAL", "pval", "p", "P", "P.2gc"],
+                "Effect_size": ["LINREG", "OR"],
+                "Minor_Allele_Freq": ["REF_FRQ", "reffrq", "Freq.Hapmap.Ceu", "Freq.Allele1.HapMapCEU", "MAF"
+                                      "Freq.Hapmap.Ceu"],
+                "Info": ["Info", "info"]
+            }
+            return header_sets
