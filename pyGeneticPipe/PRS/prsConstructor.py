fdb77c6460219a7198a3e528660a534c69e11c41
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-10-24 15:34:58 +0100

    remove information from hdf5 in public creator

--- pyGeneticPipe/PRS/prsConstructor.py
+++ pyGeneticPipe/PRS/prsConstructor.py
@@ -1,14 +1,10 @@
 from pyGeneticPipe.PRS.Summary import Summary
 from pyGeneticPipe.utils.Input import Input
-from pathlib import Path
-import h5py
 
 
 class PRSConstructor(Summary, Input):
     def __init__(self, json_args):
         super().__init__(json_args)
-
-        self.hdf5_file = h5py.File(Path(self.working_dir, self.project_name), "w")
 
     def prs_from_summary(self):
         self.clean_summary_data()
