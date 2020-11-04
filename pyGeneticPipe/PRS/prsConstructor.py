6bd0ff739532a896edbec9945696522c88d5927b
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-11-04 10:59:05 +0000

    Clean summary stats, allow for custom names although currently private

--- pyGeneticPipe/PRS/prsConstructor.py
+++ pyGeneticPipe/PRS/prsConstructor.py
@@ -1,10 +1,24 @@
+from pyGeneticPipe.utils.misc import terminal_time
 from pyGeneticPipe.PRS.Summary import Summary
 from pyGeneticPipe.utils.Input import Input
+from pathlib import Path
 
 
 class PRSConstructor(Summary, Input):
     def __init__(self, json_args):
         super().__init__(json_args)
 
+        self._clean_summary_name = "Summary_stats"
+
     def prs_from_summary(self):
-        self.clean_summary_data()
+        print(f"Starting PRS construction {terminal_time()}")
+
+        # First clean the summary statistics
+        if not Path(self.working_dir, f"{self.project_name}_{self._clean_summary_name}"):
+            print("Cleaning GWAS summary statistics")
+            self.clean_summary_data(self._clean_summary_name)
+        else:
+            print(f"Found, so will use, pre-existing Summary statistics for project: {self.project_name}")
+
+        # Now we process the summary statistics file
+
