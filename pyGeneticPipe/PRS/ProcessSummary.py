ca8b2ba012ffdbc1b75b8e2fcd4ae5c1c0cd280d
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-11-11 14:14:54 +0000

    Non validation start loop

--- pyGeneticPipe/PRS/ProcessSummary.py
+++ pyGeneticPipe/PRS/ProcessSummary.py
@@ -8,33 +8,48 @@ class ProcessSummary(Input):
     def __init__(self, args):
         super().__init__(args)
 
+        self._bim_by_chromosomes = self._set_bim_by_chromosomes()
+        # self._validation_bim_by_chromosomes = self._set_validation_file()
+
     def pre_process_plink(self, hdf5_path):
 
-        # Extract the loci
-        bim_loci = PlinkObject(self.args["LD_Reference_Genotype"]).bim_object()
+        # Open the cleaned summary statistics
+        summary_file = h5py.File(hdf5_path, "r")["Sum_Stats"]
 
-        # Extract the unique sorted chromosomes
-        valid_chromosomes = np.unique([int(loci.chromosome) for loci in bim_loci])
-        valid_chromosomes.sort()
+        for chrom_bim in self._bim_by_chromosomes:
 
-        # ssf = h5py.File(hdf5_path, "r")["Sum_Stats"]
-        #
-        # for chromosome in valid_chromosomes:
-        #     print(chromosome)
-        #     chromosome_group = ssf[str(chromosome)]
-        #
-        #     chromosome_snp_ids = (chromosome_group['snp_id'][...]).astype('<U30')
-        #
-        #     # A map from sid to index for summary stats
-        #     ss_sid_dict = {}
-        #     for i, sid in enumerate(chromosome_snp_ids):
-        #         ss_sid_dict[sid] = i
-        #
-        #     print(chromosome_snp_ids)
-        #     print(ss_sid_dict)
-        #     break
+            # Extract the summary statistics data for this chromosome, if we fail to extract it continue
+            chrom_summary = self._summary_data_by_chromosome(summary_file, chrom_bim)
+            if not chrom_summary:
+                continue
+
+            print(chrom_summary)
+            break
 
     def pre_process_bgen(self):
         raise NotImplementedError("Bgen files not yet implemented")
+
+    @staticmethod
+    def _summary_data_by_chromosome(summary_file, chrom_bim):
+        """
+        Try to extract the data by chromosome, if we failed to extract it summary statistics file return None so it will
+        not crash and warn the user this has happened.
+        """
+        try:
+            return summary_file[chrom_bim.chromosome]
+        except KeyError:
+            print(f"WARNING: Failed to find {chrom_bim.chromosome} in summary statistics file")
+            return None
+
+    def _set_bim_by_chromosomes(self):
+        """
+        Extract the information in the bim file but by chromosomes
+        """
+        # todo: will need to be generalised for validation
+        # Create a plink Objected
+        plink_obj = PlinkObject(self.args["LD_Reference_Genotype"])
+
+        # Extract the information by chromosomes
+        return plink_obj.bim_by_chromosome()
 
 
