ee29a9da18e7d21a81e4e6c25cc589171eb4e9fb
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-11-17 16:12:51 +0000

    Directory iteration

--- pyGeneticPipe/utils/misc.py
+++ pyGeneticPipe/utils/misc.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import gzip
+import os
 
 
 def bits_to_int(bits):
@@ -44,3 +45,24 @@ def terminal_time():
     A way to remember when you initialised a cell by return the current hour and minute as a string
     """
     return f"{datetime.now().time().hour}:{datetime.now().time().minute}"
+
+
+def directory_iterator(directory, file_only=True):
+    """
+    This takes a directory and returns a list of entries within that directory, if file_only is selected only
+    files as apposed to directories will be returned
+
+    :param directory: The directory you wish to iterate through
+    :type directory: str
+
+    :param file_only: Defaults to true where the return is just a list of files
+    :type file_only: bool
+
+    :return: List of entries from the directory
+    :rtype: list
+    """
+
+    if file_only:
+        return [file for file in os.listdir(directory) if os.path.isfile(f"{directory}/{file}")]
+    else:
+        return [file for file in os.listdir(directory)]
