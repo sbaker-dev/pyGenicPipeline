e6b28388dd42650a4dfc63f746dd99d8e2ea8d5f
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-11-18 17:25:11 +0000

    Add tests for create_validation_group via its yaml config

--- FormalTests/CommandTests.py
+++ FormalTests/CommandTests.py
@@ -1,6 +1,8 @@
 import unittest
 import subprocess
 from pathlib import Path
+from pyGeneticPipe.clean.Cleaner import Cleaner
+import h5py
 
 
 class MyTestCase(unittest.TestCase):
@@ -35,6 +37,43 @@ class MyTestCase(unittest.TestCase):
         assert log_file.exists(), "plink failed to produce toy_data.log"
         print(f"Found {log_file.name}: Deleting")
         log_file.unlink()
+
+    def test_cvg_bed(self):
+        """Test the create_validation_group for .bed"""
+        testing_path = Path(__file__).parent.absolute()
+
+        # For .bed (within config)
+        bed_handel = Cleaner(Path(testing_path, "create_validation_group.yaml"))
+        bed_handel.load_type = ".bed"
+        print("Validating for .bed")
+        self._validate_cvg(bed_handel)
+
+    def test_cvg_bgen(self):
+        """Test the create_validation_group for .bgen"""
+        testing_path = Path(__file__).parent.absolute()
+
+        # For .bgen (within config)
+        bgen_handel = Cleaner(Path(testing_path, "create_validation_group.yaml"))
+        print("Validating for .bgen")
+        self._validate_cvg(bgen_handel)
+
+    def _validate_cvg(self, job_handel):
+        """
+        We found an error that .bgen files load [rs123, rs123] yet we where expect rs123. When this happended we ended
+        up with length issues as well as the problems associate with the snps. This tests for both of these conditions
+        and is generalised to test on .bgen and .bed
+        """
+        job_handel.create_validation_group()
+
+        with h5py.File(Path(job_handel.working_dir, job_handel.project_name), "r") as f:
+
+            self.assertEqual(f["Validation"]["Valid_Snps"].shape[0], 540528)
+
+            # Check we found all chromosomes of sample file
+            self.assertEqual(f["Validation"]["Valid_Chromosomes"].shape[0], 23)
+
+            # Check that we only have the snp name once
+            self.assertEqual(len(f["Validation"]["Valid_Snps"][0].decode().split(",")), 1)
 
 
 if __name__ == '__main__':
---
+++ FormalTests/README.md
@@ -0,0 +1,11 @@
+### UnitTesting
+
+This...well it more or less violates the principles of unit Testing, but its as close as this process has. Methods, 
+processes, and other parts of the program are test here, with arguments read in via yaml files just like the program it
+self would be most of the time. 
+
+Tests require data from [this example set][exd] (download at the end of the first paragraph). If you want to test these
+you will need to change the config files via each .yaml or generate them yourself via pyGeneticPipe\support\ArgMaker
+
+[exd]: https://choishingwan.github.io/PRS-Tutorial/target/
+
