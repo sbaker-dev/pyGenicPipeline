f3c9f3a5a0cea28b65c9cc7db5fd1ee1d7b7eab9
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-11-26 16:23:11 +0000

    Inital colorma

--- pyGeneticPipe/core/Main.py
+++ pyGeneticPipe/core/Main.py
@@ -2,6 +2,7 @@ from pyGeneticPipe.support.ShellMaker import ShellMaker
 from pyGeneticPipe.utils.misc import terminal_time
 from pyGeneticPipe.clean.Cleaner import Cleaner
 from pyGeneticPipe.core.Input import Input
+from colorama import init
 
 
 class Main(ShellMaker, Cleaner, Input):
@@ -10,6 +11,7 @@ class Main(ShellMaker, Cleaner, Input):
         This Class inherits all other classes that can be used, and then execute the job via getattr
         :param args: Json args that have been set via GUI or manually
         """
+        init(autoreset=True)
         super().__init__(args)
         print(f"Starting {self.operation}: {terminal_time()}")
         getattr(Main, self.operation)(self)
