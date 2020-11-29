25c3824def379210d1adc7a251e7c265f568edec
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-11-29 11:40:48 +0000

    create disequlbirum

--- pyGeneticPipe/clean/adjust.py
+++ pyGeneticPipe/clean/adjust.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import h5py
-
+import sys
 import numpy as np
 
 
@@ -12,7 +12,7 @@ def standardise_snps(g):
     raw_snps = g['raw_snps_ref'][...]
     snp_stds = g['snp_stds_ref'][...]
     snp_means = g['snp_means_ref'][...]
-    n_snps = raw_snps.shape[0]
+    n_snps, n_individuals = raw_snps.shape
 
     # Need to reformat the shape to construct snps
     snp_means.shape = (n_snps, 1)
@@ -21,20 +21,95 @@ def standardise_snps(g):
     snps = np.array((raw_snps - snp_means) / snp_stds, dtype="float32")
 
     assert snps.shape == raw_snps.shape
-    return snps, n_snps
+    return snps, n_snps, n_individuals
 
 
-def call_main(coord_file):
+def shrink_r2_matrix(distance_dp, n):
+    """
+    The values of R squared to be no less or greater than -1 and 1 respectively so the value calculated from the dot
+    product is bounded to -1 and 1. Any value that is less than 1 divided by the number of individuals minus 1
+    (to account for 0 indexing) is set to zero to reduce the complexity of the finalised r squared matrix
+
+    :param distance_dp: The dot product calculated from the snps in ld and the transposed matrix of snps account for the
+        number of individuals in the sample
+    :param n: Then umber of individuals in the sample
+    :return: Clipped and reduced r2 matrix
+    """
+
+    clipped_r2 = np.clip(distance_dp, -1, 1)
+    clipped_r2[np.absolute(clipped_r2) < (1.0/(n-1))] = 0
+    return clipped_r2
+
+
+def _calculate_disequilibrium(snps, snp_i, snp, n_individuals, ld_dict, ld_scores):
+    """
+    This (i belie...the method was just called _calculate_D) calculates the disequilibrium of the current snps in ld
+
+    :param snps: Normalised snps around the current normalised snp with a maximum length of (radius * 2) + 1
+    :param snp_i: Current snp index
+    :param snp: Current normalised snp
+    :param n_individuals: Number of individuals in the sample
+    :param ld_dict: dict for storing shrunk r2 matrix
+    :param ld_scores: list for storing r2 matrix
+    :return: Nothing, append to dict and list of ld_dict and ld_scores
+    """
+
+    distance_dp = np.dot(snp, snps.T) / n_individuals
+
+    ld_dict[snp_i] = shrink_r2_matrix(distance_dp, n_individuals)
+
+    r2s = distance_dp ** 2
+    ld_scores[snp_i] = np.sum(r2s - ((1 - r2s) / (n_individuals - 2)), dtype='float32')
+
+
+def snps_in_ld(snps, snp_index, radius, number_of_snps):
+    """
+    We want to construct a window of -r + r around each snp where r is the radius. However, the first r and last N-r of
+    the snps will not have r number of snps before or after them so we need to account for this by:
+
+    Taking the maximum of (0, i-r) so that we never get a negative index
+    Taking the minimum of (n_snps, (i + radius + 1)) to ensure we never get an index out of range
+
+    :param snps: Normalised snps
+    :param snp_index: Index
+    :param radius: radius
+    :param number_of_snps: total number of snps
+
+    :return: An array of shape snps of a maximum of 'radius' number of snps surrounding the current snp accessed via
+        index.
+    """
+    return snps[max(0, snp_index - radius): min(number_of_snps, (snp_index + radius + 1))]
+
+
+def call_main(coord_file, radius):
     df = h5py.File(coord_file, 'r')
     cord_data_g = df['cord_data']
 
+    tt = 0
     for chrom_str in cord_data_g:
         g = cord_data_g[chrom_str]
 
         # todo This is just a standardised mesure that we could have calculated in input
-        snps, n_snps = standardise_snps(g)
+        snps, n_snps, n_individuals = standardise_snps(g)
+
+        ld_dict = {}
+        ld_scores = np.ones(n_snps)
+
+        # todo genetic map seems to be a thing, we can look into that later
+        # If we don't have a genetic map
+        # compute disequilibrium.
+        for i, snp in enumerate(snps):
+            _calculate_disequilibrium(snps_in_ld(snps, i, radius, n_snps), i, snp, n_individuals, ld_dict, ld_scores)
+
+
+
 
         break
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -42,7 +117,7 @@ if __name__ == '__main__':
     fff = "EUR.ld"
     rr = 183
 
-    call_main(cf)
+    call_main(cf, rr)
 
 
 
