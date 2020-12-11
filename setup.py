c0c8bc78abb63884f30b5431eb857edab0e01776
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-12-11 14:05:20 +0000

    Logging keys after filter

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/pyGenicPipeline"
-VERSION = "0.02.11"
+VERSION = "0.02.12"
 PYTHON_REQUIRES = ">=3.7"
 
 INSTALL_REQUIRES = [
