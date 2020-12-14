1ba8e6f1abfec2e8785f048b44c5658f6ec6ced8
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-12-14 15:45:10 +0000

    completed first full pass of external data

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/pyGenicPipeline"
-VERSION = "0.07.5`"
+VERSION = "0.08.0`"
 PYTHON_REQUIRES = ">=3.7"
 
 # todo Pipe install pyGenicPipe
