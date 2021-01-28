3866a5e4ecf1dcecf80e5fee3e0d4f79caddd23a
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-01-28 09:54:46 +0000

    Simplify write_yaml_group args to take a parsed dict rather than parse it within itself

--- pyGenicPipeline/utils/ArgMaker.py
+++ pyGenicPipeline/utils/ArgMaker.py
@@ -39,18 +39,18 @@ class ArgMaker:
 
         # Write Mandatory args
         self._write_header(file, "MANDATORY ARGS - IF ANY ARE NONE JOB WILL NOT RUN")
-        self._write_args(file, args_dict, "Mandatory")
+        self._write_args(file, args_dict["Mandatory"])
 
         # Write Optional args
         self._write_header(file, "OPTIONAL ARGS - NOT STRICTLY REQUIRED TO RUN BUT ALTERS OPERATION")
-        self._write_args(file, args_dict, "Optional")
+        self._write_args(file, args_dict["Optional"])
 
         # Write require to run the system, but needed for this operation
         self._write_header(file, "SYSTEM ARGS - WILL NOT BE USED FOR THIS OPERATION AND CAN STAY AS NULL BUT REQUIRED "
                                  "TO EXIST IN FILE")
         loaded_keys = list(args_dict["Mandatory"].keys()) + list(args_dict["Optional"].keys())
         args_dict["System"] = {key: None for key in self.all_args if key not in loaded_keys}
-        self._write_args(file, args_dict, "System")
+        self._write_args(file, args_dict["System"])
 
         # Finish the file
         file.close()
@@ -105,17 +105,24 @@ class ArgMaker:
         """Isolate the keys from all operational args that we need for this operation"""
         return {key: value for key, value in zip(self.all_args.keys(), self.all_args.values()) if key in operation_keys}
 
-    def _write_header(self, file, message):
-        """Write a header with a message and trailing # up to the line length"""
-        file.write(f"# {message} " + f"".join(["#" for _ in range(self.line_width - (len(message) + 3))]) + "\n\n")
+    def _write_args(self, file, args_dict, spacing=0, write_descriptions=True):
+        """
+        For a given sub type of args, write the value if set of null otherwise
 
-    def _write_args(self, file, args_dict, args_type, spacing=0, write_descriptions=True):
-        """For a given sub type of args, write the value if set of null otherwise"""
-        for key, value in zip(args_dict[args_type].keys(), args_dict[args_type].values()):
+        Args are group into mandatory, optional, and system. This takes the file object, and the dict of keys of a given
+        group to iterate though. If write descriptions are set, then for each key in the dict a set of lines explaining
+        what the key means. Then, if a value has been set that is not None, the value is writen to the file else null
+        which is yaml for None.
+
+        A Space in yaml represents a tab in most other languages, and spacing can be set to values above zero if you
+        want to create items within dict for example.
+        """
+        for key, value in zip(args_dict.keys(), args_dict.values()):
             if write_descriptions:
                 self._write_description(file, self._arg_descriptions[key])
 
-            if value:
+            # To ensure Boolean False is not set to null
+            if value is not None:
                 file.write(f"{''.join([' ' for _ in range(spacing)])}" + f"{key}: {value}\n\n")
             else:
                 file.write(f"{''.join([' ' for _ in range(spacing)])}" + f"{key}: null\n\n")
