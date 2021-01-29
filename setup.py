4b8c3e8d6608a09e6b2ca1a40c64752f54baab15
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-01-29 11:10:25 +0000

    Tweaking shell controllers

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/pyGenicPipeline"
-VERSION = "0.09.0`"
+VERSION = "0.09.1`"
 PYTHON_REQUIRES = ">=3.7"
 
 INSTALL_REQUIRES = [
